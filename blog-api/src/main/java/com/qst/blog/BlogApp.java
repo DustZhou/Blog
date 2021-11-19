7896fbd80c19ffe380db5da8d95719aa0c859267
DustZhou <zht0426@gmail.com>
2021-11-19 17:42:05 +0800

    Blog第一次提交

---
+++ blog-api/src/main/java/com/qst/blog/BlogApp.java
@@ -0,0 +1,9 @@
+package com.qst.blog;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+@SpringBootApplication
+public class BlogApp {
+    public static void main(String[] args){
+        SpringApplication.run(BlogApp.class,args);
+    }
+}
